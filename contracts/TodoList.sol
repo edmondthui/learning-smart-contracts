e207ec93a2f60e19145f4043a677f5195164d262
edmondthui <edmond.t.hui@gmail.com>
2022-03-13 01:53:51 -0500

    feat: load account and tasks from block chain

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0; // public automatically creates a getter for this taskCount
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks; // takes a key value pair and is similar to a hash in javascript
+
+    constructor() public { // this runs when the start contract is being deployed for the first time
+        createTask("Edmond is the best"); // this creates a default task to the TodoList 
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
---
+++ src/app.js
@@ -0,0 +1,95 @@
+App = {
+    loading: false,
+    contracts: {},
+
+    load: async() => {
+        if (typeof window.ethereum !== 'undefined') {
+            console.log('MetaMask is installed!');
+            await App.loadAccount();
+            await App.loadContract();
+            await App.render();
+        } else {
+            // if metamask isn't installed do something
+        }
+    },
+
+    loadAccount: async () => {
+        const account = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        .catch((e) => {
+            console.error(e.message);
+            return;
+        })
+        App.account = account[0];
+        console.log(App.account);
+    },
+
+    loadContract: async() => {
+        const todoList = await $.getJSON('TodoList.json'); // inside bs-config file we expose build contract directory so we have access to this file
+        console.log(todoList);
+        App.contracts.TodoList = TruffleContract(todoList);
+        App.contracts.TodoList.setProvider(window.web3.currentProvider)
+        App.todoList = await App.contracts.TodoList.deployed();
+    },
+
+    render: async() => {
+        if (App.loading) {
+            return
+        }
+        App.toggleLoading(true);
+        $('#account').html(App.account);
+
+        // render tasks
+        await App.renderTasks();
+
+        App.toggleLoading(false);
+    },
+
+    renderTasks: async() => {
+        const taskCount = await App.todoList.taskCount();
+        const taskTemplate = $('.taskTemplate');
+
+        for (let i = 0 ; i < taskCount; i++) {
+            // fetching task data from the blockchain for each task
+            const task = await App.todoList.tasks(i);
+            const taskId = task[0].toNumber();
+            const taskContent = task[1];
+            const taskCompleted = task[2];
+
+            const newTaskTemplate = taskTemplate.clone();
+            newTaskTemplate.find('content').html(taskContent);
+            newTaskTemplate.find('input')
+            .prop('name', taskId)
+            .prop('checked', taskCompleted)
+            // .on('click', App.toggleCompleted)
+
+            if (taskCompleted) {
+                $('#completedTaskList').append(newTaskTemplate);
+            } else {
+                $('#taskList').append(newTaskTemplate);
+            }
+            newTaskTemplate.show();
+        }
+
+    },
+
+    toggleLoading: (boolean) => {
+        App.loading = boolean;
+        const loader = $('#loader');
+        const content = $("#content");
+
+        if (boolean) {
+            loader.show();
+            content.hide();
+        } else {
+            loader.hide();
+            content.show();
+        }
+    }
+}
+
+
+$(() => {
+    $(window).load(()=> {
+        App.load()
+    })
+})
